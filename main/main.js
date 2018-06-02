7a84f871c17b2b4cd4c8e35266712979de48eabd
DingGe520R <784872490@qq.com>
2018-06-02 13:09:41 +0800

    1

--- main/main.js
+++ main/main.js
@@ -1,4 +1,20 @@
-module.exports = function main() {
-    console.log("Debug Info");
-    return 'Hello World!';
+
+
+
+module.exports = function main(kilometer, stoptime) {
+
+    var sum = 0;
+    if (kilometer <= 2) {
+        sum = 6;
+    }
+    else if (kilometer <= 8 && kilometer >= 2) {
+        sum += 6 + ((kilometer - 2) * 0.8);
+    }
+    else if (kilometer > 8) {
+        var a = kilometer - 8;
+        var b = kilometer - a - 2;
+        sum += 6 + (a * 1.2) + (b * 0.8);
+    }
+    sum += stoptime * 0.25;
+    return Math.round(sum);
 };
--- spec/main-spec.js
+++ spec/main-spec.js
@@ -2,4 +2,11 @@ const main = require('../main/main');
 
 describe('taxi fee', function () {
     // write your tests here...
+    var kilometer = 10;
+    var stoptime = 2;
+    it('根据里程以及停车时间求出车费', function () {
+
+        var result = main(kilometer, stoptime);
+        expect(result).toEqual(14);
+    });
 });
